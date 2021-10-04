c6aa547aa1c95234a283940bd43bf206a61e4bd3
azalialoe <azalia.loeciano@gmail.com>
2021-10-04 12:25:59 -0400

    another trial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>trial bruh</h1>
-        <img src={logo} className="App-logo" alt="logo" />
+      
+        <h1>trial this is</h1>
+       
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          i mean sure i guess
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <p>this is fine</p>
+        
     </div>
   );
 }
